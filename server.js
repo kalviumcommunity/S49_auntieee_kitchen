9f8116e0c978d31e7ddbcc645faff59d1cbe6feb
manaswinin236 <141708171+manaswinin236@users.noreply.github.com>
2024-02-16 10:45:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/ping', (req, res) => {
-
   res.send('pong');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-
 });
