fcf26f7885749efd2167bf067d987d816fdd13ef
manaswini <manaswini0623@gmail.com>
2024-03-07 09:42:07 +0530

    created the landing page and added gitignore file in backend folder

---
+++ client/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css"
+function Header(){
+    return(
+        <>
+        <header>
+            <h1>THE TASTE OF HOMEMADE FOOD</h1>
+            <h4>THE MODERN INDIAN AUNTIE'S KITCHEN</h4>
+            <button id="explore">Explore Dishes</button>
+        </header>
+        </>
+    )
+}
+
+export default Header
