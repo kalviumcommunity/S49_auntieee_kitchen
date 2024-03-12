8518df9a4c7b9c6a36d897b6b3c9e83c45b12b17
manaswini <manaswini0623@gmail.com>
2024-03-12 12:24:38 +0530

    updates to fix the error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Userdata from './components/displayUsers';
 import LandingPage from './components/landingpage';
 import UpdateUsers from './components/updateUsers';
-import CreateUsers from './components/createUsers';
+import CreateUsers from './components/createUser';
 
 
 function App() {
