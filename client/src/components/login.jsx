110cfdc5344ec1da116ecda5caa71e01517ba1bf
manaswini <manaswini0623@gmail.com>
2024-04-01 15:47:25 +0530

    installed js-cookie package and made changes in the login.jsx page to store the login information in the form of a cookie

---
+++ client/src/components/login.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./feature.css";
+import axios from "axios";
+import Cookies from 'js-cookie';
+import { useNavigate } from "react-router-dom";
+
+function LoginUser() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:3000/api/loginUser", { username, password });
+            console.log(response);
+            Cookies.set('username', username, { expires: 7 });
+            navigate("/landingPage");
+        } catch (err) {
+            if (err.response && err.response.status === 401) {
+                setError("Username or password is incorrect.");
+            } else {
+                console.log(err);
+            }
+        }
+    };
+
+    return (
+        <div id="login">
+            <form onSubmit={handleSubmit}>
+                <h2 id="login-heading">Login</h2>
+                <div id="login-content">
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <br />
+                        <input
+                            type="text"
+                            placeholder="Enter username"
+                            className="input-field"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <br />
+                        <input
+                            type="password"
+                            placeholder="Enter password"
+                            className="input-field"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    {error && <div className="error">{error}</div>}
+                    <div id="log">
+                        <button id="login-btn">
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default LoginUser;
