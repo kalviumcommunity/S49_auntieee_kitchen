220c322fce94241287a7a5d956bfb835ad4ef407
manaswini <manaswini0623@gmail.com>
2024-04-02 05:32:33 +0530

    error handling

--- client/src/components/logoutButton.jsx
+++ client/src/components/logoutButton.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import Cookies from 'js-cookie';
 import "./navbar.css"
-import {useNavigate,Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const LogoutButton = () => {
 
